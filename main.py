a551a7648e294b7274f13b4e8eef5c555670686b
Jonathan Steadman <52569279+ToothedTomb@users.noreply.github.com>
2023-12-16 10:58:13 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 # pip install tkinter
+import sqlite3
 from tkinter import * 
 # pip install pyperclip
 import pyperclip 
@@ -9,7 +10,7 @@ from tkinter import messagebox
 import tkinter as ttk
 import tkinter as tk
 root = Tk()
-root.geometry("700x300")
+root.geometry("700x330")
 root.resizable(0,0)
 root.title("OpenPasswordGen")
 my_menu= Menu(root)
@@ -18,6 +19,13 @@ root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='Icon.png'))
 
 #Popups that will display.
 #The popup for what this software is.
+#Creating a database 
+Database_init = sqlite3.connect("Passwords.db")
+cursor = Database_init.cursor()
+
+#Just creating a simple table.
+cursor.execute('''CREATE TABLE IF NOT EXISTS passwords (id INTEGER PRIMARY KEY, password TEXT)''')
+Database_init.commit()
 def About():
     root = Tk() 
     root.resizable(0,0)
@@ -29,8 +37,6 @@ def About():
 
     labelTitle.pack(side="top",fill="x",pady=1)
     label.pack(side="top", fill="x", pady=2)
-    B1 = tk.Button(root, text="Exit",font=("ubuntu",28),bg="grey",border=12,activebackground='pink', command = root.destroy)
-    B1.pack()
 #############
 #This popup is used to show who created this software.
 def Who_made_Me():
@@ -44,13 +50,44 @@ def Who_made_Me():
 
     labelTitle.pack(side="top",fill="x",pady=1)
     label.pack(side="top", fill="x", pady=2)
-    B1 = tk.Button(root, text="Exit",font=("ubuntu",28),bg="grey",border=12,activebackground='pink', command = root.destroy)
-    B1.pack()
+
+
+def load_passwords_popup():
+    cursor.execute("SELECT password FROM passwords")
+    loaded_passwords = [row[0] for row in cursor.fetchall()]
+
+    popup = Toplevel(root)
+    popup.title("All Passwords in Database")
+
+    labelTitle = Label(popup, font=("Ubuntu", 18, "bold", "underline"), anchor='center', text="All Passwords:")
+    labelTitle.pack(side="top", fill="x", pady=10)
+
+    for password in loaded_passwords:
+        frame = Frame(popup)
+        frame.pack(side="top", fill="x", pady=5)
+
+        label = Label(frame, font=("Ubuntu", 14), anchor='center', text=password)
+        label.pack(side="left", padx=10)
+
+        copy_button = Button(frame, text="Copy", font=("Ubuntu", 12), bg="grey", activebackground='pink', command=lambda p=password: pyperclip.copy(p))
+        copy_button.pack(side="right", padx=10)
+def save_database_password():
+    password_save = passwrd.get().splitlines()
+    for password in password_save:
+        cursor.execute("INSERT INTO passwords (password) VALUES (?)", (password,))
+    Database_init.commit()
+    messagebox.showinfo("It worked", "Password saved.")
+
 file_menu= Menu(my_menu,background="grey",activebackground="pink",border="6")
 
 my_menu.add_cascade(label="About:",font=("Ubuntu",18),activebackground="pink", menu=file_menu)
 file_menu.add_command(label="What is this software?",font=("Ubuntu",18),activebackground="pink",background="grey",command=About) 
 file_menu.add_command(label="Who made this software?",font=("Ubuntu",18),activebackground="pink",background="grey",command=Who_made_Me) 
+Database = Menu(my_menu,background="grey",activebackground="pink",border="6")
+
+my_menu.add_cascade(label="Options:",font=("Ubuntu",18),activebackground="pink", menu=Database)
+Database.add_command(label="Save to Database.",font=("Ubuntu",18),activebackground="pink",background="grey",command=save_database_password)
+Database.add_command(label="Load Database.",font=("Ubuntu",18),activebackground="pink",background="grey",command=load_passwords_popup)
 
 passwrd = StringVar()
 passlen = IntVar()
@@ -78,7 +115,7 @@ def copyclipboard():
     random_password = passwrd.get()
     pyperclip.copy(random_password)
 # Labels
- 
+
  
 Label(root, text="Open source Password Generator!", font=" ubuntu 30 bold underline").pack()
 Label(root, text="Enter how many characters do you want your password to be:", font="ubuntu 14 bold").pack(pady=3)
